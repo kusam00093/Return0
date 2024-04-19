e372897ac4a56ba104e0df6f29b0cdb8dd18d3e7
kusam00093 <yusanghun12@naver.com>
2024-04-19 17:07:42 +0900

    d

--- src/main/java/com/board/boardpaging/controller/BoardPagingController.java
+++ src/main/java/com/board/boardpaging/controller/BoardPagingController.java
@@ -23,7 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("/BoardPaging")
 public class BoardPagingController {
-
+ 
 	@Autowired
 	private BoardPagingMapper boardPagingMapper;
 	
